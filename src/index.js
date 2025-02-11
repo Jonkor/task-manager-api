d2629caf6dbf7a42506f6a2722e58b54425520f7
Jonkor <juan.kucho@hotmail.com>
2025-02-11 01:30:25 -0800

    refactored endpoiunts with async/await

--- src/index.js
+++ src/index.js
@@ -8,24 +8,79 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.post('/usuarios', (req, res) => {
+app.post('/usuarios', async (req, res) => {
     const usuario = new Usuario(req.body);
 
-    usuario.save().then(() => {
+    try {
+        await usuario.save();
         res.status(201).send(usuario);
-    }).catch((e) => {
-        res.status(400).send(e);
-    })
+    }catch (e) {
+        res.status(400).send(e);        
+    }    
 })
 
-app.post('/tareas', (req, res) => {
+app.get('/usuarios', async (req, res) => {
+
+    try {
+        const usuarios = await Usuario.find({});
+        res.send(usuarios);
+    }catch (e){
+        res.status(500).send();
+    }
+    
+})
+
+app.get('/usuarios/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const usuario = await Usuario.findById(_id);
+        if (!usuario){
+            return res.status(404).send();
+        }
+        res.send(usuario); 
+    }catch (e){
+        res.status(500).send();
+    }
+
+})
+
+app.post('/tareas', async (req, res) => {
     const tarea = new Tarea(req.body);
 
-    tarea.save().then(() =>{
-        res.status(201).send(tarea);        
-    }).catch((error) => {
-        res.status(400).send(error);
-    })
+    try {
+        await tarea.save();
+        res.status(201).send(tarea);
+    }catch (e){
+        res.status(400).send();
+    }
+
+})
+
+app.get('/tareas', async (req, res) => {
+
+    try {
+        const tareas = await Tarea.find({});
+        res.send(tareas);
+    }catch (e){
+        res.status(500).send();
+    }
+
+})
+
+app.get('/tareas/:id', async (req, res) => {
+    const _id = req.params.id;
+
+    try {
+        const tarea = await Tarea.findById(_id);
+        if (!tarea){
+            return res.status(404).send();
+        }
+        res.send(tarea);
+    } catch (e){
+        res.status(500).send();
+    }
+
 })
 
 app.listen(port, () => {
