f31bc95b3d7f01e41dc5ea0e99d61e33ff83b4d3
Jonkor <juan.kucho@hotmail.com>
2025-04-19 11:37:02 -0700

    add tasks sorting feature

--- src/routers/tarea.js
+++ src/routers/tarea.js
@@ -20,9 +20,15 @@ router.post('/tareas', auth, async (req, res) => {
 
 router.get('/tareas', auth, async (req, res) => {
     const match = {};
+    const sort = {};
 
     if (req.query.completado) {
         match.completado = req.query.completado === 'true';
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
     }
 
     try {
@@ -31,7 +37,8 @@ router.get('/tareas', auth, async (req, res) => {
             match,
             options: {
                 limit: parseInt(req.query.limit),
-                skip: parseInt(req.query.skip)
+                skip: parseInt(req.query.skip),
+                sort
             }
         });
         res.send(req.usuario.tareas);
